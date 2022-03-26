f3960ce374cf5fb466b5889d2f29d562e2d545f8
martin herrlein <herrlein.martin@gmail.com>
2022-03-26 10:35:31 -0300

    verPerfil

---
+++ routers/procesoToken.go
@@ -0,0 +1,46 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/MartinHerrlein/twittor/bd"
+	"github.com/MartinHerrlein/twittor/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+/*Email valor de Email usado en todos los EndPoints*/
+var Email string
+
+/*IDUsuario es el ID devuelto del model, que se usará en todos los EndPoints*/
+var IDUsuario string
+
+/*ProcesoToken proceso token para extraer sus valores*/
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+	miClave := []byte("MastersDelDesarrollo_grupoDeFacebook")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de tokeninvalido")
+	}
+
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token invalido")
+	}
+	return claims, false, string(""), err
+}
